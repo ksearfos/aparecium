aa67b9bd731d567879e54514b527d0a97c3ce558
ksearfos <ksearfos@covermymeds.com>
2018-12-06 10:55:47 -0500

    clean up logic a bit

--- lib/aparecium.rb
+++ lib/aparecium.rb
@@ -3,32 +3,36 @@ require 'aparecium/data_file'
 class Aparecium
   ROOT = Pathname.new(__FILE__).parent.parent
 
-  def initialize
-    @counter = 0
-    @dependencies = []
+  attr_reader :dependencies
+
+  def initialize(file)
+    @file = DataFile.new(file)
   end
 
-  def all_dependencies(file)
-    file = DataFile.new(file)
-    immediate_deps = file.dependencies_with_paths
-
-    # get list of all immediate dependencies
-    # for each of those, add all of its immediate dependencies
-    # keep going until we run out of dependencies
-    # and don't add the same dependency twice
-    immediate_deps.each do |name, file|
-      @counter += 1
-      next if @dependencies.include?(name) || @counter > 10
-      @dependencies << name
-      @dependencies += all_dependencies(full_path(file))
-    end
-
-    @dependencies.uniq
+  def all_dependencies()
+    recursive_dependencies(@file, []).uniq
   end
 
   private
 
+  def data_file(partial_path)
+    DataFile.new(full_path(partial_path))
+  end
+
   def full_path(file)
     ROOT.join('spec', 'support', 'fixtures', file)
+  end
+
+  def recursive_dependencies(file, known_dependencies)
+    immediate_deps = file.dependencies_with_paths
+
+    immediate_deps.each do |name, filepath|
+      next if known_dependencies.include?(name)  # no infinite recursion!
+  
+      known_dependencies << name
+      known_dependencies += recursive_dependencies(data_file(filepath), known_dependencies)
+    end
+
+    known_dependencies
   end
 end
--- spec/lib/aparecium_spec.rb
+++ spec/lib/aparecium_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
 RSpec.describe Aparecium do
+  subject { described_class.new(file) }
+
   describe '#all_dependencies' do
     context 'when there are no dependencies' do
       let(:file) { FixtureHelper::EMPTY_FIXTURE }
 
       specify do
-        expect(subject.all_dependencies(file)).to be_empty
+        expect(subject.all_dependencies).to be_empty
       end
     end
 
@@ -14,7 +16,7 @@ RSpec.describe Aparecium do
       let(:file) { FixtureHelper::NONRECURSIVE_FIXTURE }
 
       it "is all of the app's dependencies" do
-        expect(subject.all_dependencies(file)).to eq FixtureHelper::NONRECURSIVE_DEPS
+        expect(subject.all_dependencies).to eq FixtureHelper::NONRECURSIVE_DEPS
       end
     end
 
@@ -22,7 +24,7 @@ RSpec.describe Aparecium do
       let(:file) { FixtureHelper::DEPS_WITH_DEPS_FIXTURE }
 
       it "is all of the app's dependencies, and their dependencies" do
-        expect(subject.all_dependencies(file)).to eq FixtureHelper::DEPS_WITH_DEPS
+        expect(subject.all_dependencies).to eq FixtureHelper::DEPS_WITH_DEPS
       end
     end
 
@@ -30,7 +32,7 @@ RSpec.describe Aparecium do
       let(:file) { FixtureHelper::FIXTURE_WITH_ALL_DEPS }
 
       it "only checks a dependency once" do
-        expect(subject.all_dependencies(file)).to match_array FixtureHelper::ALL_DEPS
+        expect(subject.all_dependencies).to match_array FixtureHelper::ALL_DEPS
       end
     end
   end
